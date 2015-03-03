83e72812b9aeefd5b6d3d76346295e1ea8ec3ecd
Jack Kalish <jykalish@gmail.com>
2015-03-03 14:13:24 -0800

    added integration of ofxwatchdog

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofxWatchdog.h"
 
 //========================================================================
 int main( ){
 
+    ofxWatchdog::watch(3000, true, true, true);
+    
 	ofSetupOpenGL(1920,1080, OF_FULLSCREEN);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
